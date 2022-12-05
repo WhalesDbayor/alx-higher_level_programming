507fc71c17f4a51046f871a27ab9395afbacb88f
WhalesDbayor <wabalonline01@gmail.com>
2022-12-05 01:55:30 +0000

    New fix

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new = ""
-    for i in my_string:
-        if i is not 'c' and i is not 'C':
-            new += i
-    return new
+	my_string = [i for i in my_string if i.upper() != 'C']
+	my_string = ''.join(my_string)
+	return (my_string)
